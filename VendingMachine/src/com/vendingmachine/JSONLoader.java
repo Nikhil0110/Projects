32e4de2f3f66b900ee79a082a43563ebc16eea01
Nikhil Mohan <Nikhil Mohan@LAPTOP-8O9KQB53>
2018-08-07 12:09:28 +0530

    first commit

---
+++ VendingMachine/src/com/vendingmachine/JSONLoader.java
@@ -0,0 +1,39 @@
+package com.vendingmachine;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+public class JSONLoader {
+	
+	static String jsonFile="N:\\WS\\VendingMachine\\Data\\product.json";
+	
+	public HashMap<String,Integer[]> loadJSON(String jsonFile) throws FileNotFoundException, IOException, ParseException{
+		Collect
+		
+		HashMap<String, Integer<>> products=new HashMap<>();
+		products.put("fanta", 20,15);
+		
+
+		
+	    
+		return null;
+		
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+
+	
+}
+	
+
+}
