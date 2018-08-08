48e7ee78b1d20522f24965b1eec5f275c2e5205d
Nikhil Mohan <Nikhil Mohan@LAPTOP-8O9KQB53>
2018-08-08 19:46:00 +0530

    first commit

--- QuizManager/src/com/testersconnect/qm/QuizLoader.java
+++ QuizManager/src/com/testersconnect/qm/QuizLoader.java
@@ -7,47 +7,31 @@ import java.io.IOException;
 
 public class QuizLoader {
 
-	public Item[] qItems=new Item[13];
-	
+	public Item[] qItems = new Item[13];
+
 	public Item[] loadQuestions(String fileName) throws IOException {
-			FileReader fr=new FileReader(System.getProperty("user.dir")+"\\Questions\\questions.csv");
-			BufferedReader br=new BufferedReader(fr);
-			int count=0;
-			String line;
-			while((line=br.readLine())!= null){
-				Item i=new Item();
-				String arr[]=line.split(",");
-				i.qid=arr[0];
-				i.question=arr[1];
-				i.options[0]=arr[2];
-				i.options[1]=arr[3];
-				i.options[2]=arr[4];
-				i.options[3]=arr[5];
-				i.ans=arr[6];
-				i.category=arr[7];
-				qItems[count]=i;
-				count++;
-				
-			}
-			
-			
-			
-		
-			return qItems;
-		
-		
+		FileReader fr = new FileReader(System.getProperty("user.dir") + "\\Questions\\questions.csv");
+		BufferedReader br = new BufferedReader(fr);
+		int count = 0;
+		String line;
+		while ((line = br.readLine()) != null) {
+			Item i = new Item();
+			String arr[] = line.split(",");
+			i.qid = arr[0];
+			i.question = arr[1];
+			i.options[0] = arr[2];
+			i.options[1] = arr[3];
+			i.options[2] = arr[4];
+			i.options[3] = arr[5];
+			i.ans = arr[6];
+			i.category = arr[7];
+			qItems[count] = i;
+			count++;
+
+		}
+
+		return qItems;
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
